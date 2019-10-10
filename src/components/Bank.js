ec15264e23ab7688ae4f7bd08bdeb14049f923ae
Tal Luigi <luigi.t.agr@gmail.com>
2019-10-10 16:59:42 -0400

    chore: removed code for code-along with students

--- src/components/Bank.js
+++ src/components/Bank.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {
-  depositFiftyActionCreator,
-  depositHundredActionCreator,
-  withdrawFiftyActionCreator,
-  withdrawHundredActionCreator,
-} from '../store/index';
 
 const Bank = props => {
   return (
@@ -21,43 +15,20 @@ const Bank = props => {
         React-Redux Bank
       </header>
       <br />
-      <h1 id="balance">$ {props.balance}</h1>
+      <h1 id="balance">$ 0</h1>
       <div className="atm">
-        <button onClick={props.depositFifty} type="button">
-          Deposit $50
-        </button>
-        <button onClick={props.depositHundred} type="button">
-          Deposit $100
-        </button>
-        <button onClick={props.withdrawFifty} type="button">
-          Withdraw $50
-        </button>
-        <button onClick={props.withdrawHundred} type="button">
-          Withdraw $100
-        </button>
+        <button type="button">Deposit $50</button>
+        <button type="button">Deposit $100</button>
+        <button type="button">Withdraw $50</button>
+        <button type="button">Withdraw $100</button>
       </div>
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  balance: state.bankReducer.balance,
-});
+const mapStateToProps = state => ({});
 
-const mapDispatchToProps = dispatch => ({
-  depositFifty() {
-    dispatch(depositFiftyActionCreator());
-  },
-  depositHundred() {
-    dispatch(depositHundredActionCreator());
-  },
-  withdrawFifty() {
-    dispatch(withdrawFiftyActionCreator());
-  },
-  withdrawHundred() {
-    dispatch(withdrawHundredActionCreator());
-  },
-});
+const mapDispatchToProps = dispatch => ({});
 
 export default connect(
   mapStateToProps,
--- src/store/bank.js
+++ src/store/bank.js
@@ -1,42 +1,15 @@
 // Initial State
-const initialState = {
-  balance: 0,
-};
+const initialState = {};
 
 // Actions
-const DEPOSIT_FIFTY = 'DEPOSIT_FIFTY';
-const DEPOSIT_HUNDRED = 'DEPOSIT_HUNDRED';
-const WITHDRAW_FIFTY = 'WITHDRAW_FIFTY';
-const WITHDRAW_HUNDRED = 'WITHDRAW_HUNDRED';
 
 // Action Creators
-export const depositFiftyActionCreator = () => ({
-  type: DEPOSIT_FIFTY,
-});
-
-export const depositHundredActionCreator = () => ({
-  type: DEPOSIT_HUNDRED,
-});
-
-export const withdrawFiftyActionCreator = () => ({
-  type: WITHDRAW_FIFTY,
-});
-
-export const withdrawHundredActionCreator = () => ({
-  type: WITHDRAW_HUNDRED,
-});
 
 // Reducer
 const bankReducer = (state = initialState, action) => {
   switch (action.type) {
-    case DEPOSIT_FIFTY:
-      return { ...state, balance: state.balance + 50 };
-    case DEPOSIT_HUNDRED:
-      return { ...state, balance: state.balance + 100 };
-    case WITHDRAW_FIFTY:
-      return { ...state, balance: state.balance - 50 };
-    case WITHDRAW_HUNDRED:
-      return { ...state, balance: state.balance - 100 };
+    // Your cases here
+
     default:
       return state;
   }
