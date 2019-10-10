97b1541e4a3e483ffdbe95eb9f9f5af40bb0cba3
Tal Luigi <luigi.t.agr@gmail.com>
2019-10-10 16:50:42 -0400

    chore: added proper spacing

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
