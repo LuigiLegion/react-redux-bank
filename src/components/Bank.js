99e437ccf827877fc79b818b8b697b2214b44d41
Tal Luigi <luigi.t.agr@gmail.com>
2019-10-17 11:50:05 -0400

    feat: added custom amount atm panel

--- src/components/Bank.js
+++ src/components/Bank.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
   depositFiftyActionCreator,
@@ -7,33 +7,64 @@ import {
   withdrawHundredActionCreator,
 } from '../store/index';
 
-const Bank = props => {
-  return (
-    <div>
-      <header>
-        <img className="logo" src="/logo.jpeg" alt="React-Redux Bank" />
+class Bank extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <img className="logo" src="/logo.jpeg" alt="React-Redux Bank" />
+
+          <br />
+
+          <div>React-Redux Bank</div>
+        </header>
+
         <br />
-        <div>React-Redux Bank</div>
-      </header>
-      <br />
-      <h1 className="balance">$ {props.balance}</h1>
-      <div className="atm">
-        <button onClick={props.depositFifty} type="button">
-          Deposit $50
-        </button>
-        <button onClick={props.depositHundred} type="button">
-          Deposit $100
-        </button>
-        <button onClick={props.withdrawFifty} type="button">
-          Withdraw $50
-        </button>
-        <button onClick={props.withdrawHundred} type="button">
-          Withdraw $100
-        </button>
+
+        <h1 className="balance">$ {this.props.balance}</h1>
+
+        <div className="atm">
+          <button onClick={this.props.depositFifty} type="button">
+            Deposit $50
+          </button>
+
+          <button onClick={this.props.depositHundred} type="button">
+            Deposit $100
+          </button>
+
+          <button onClick={this.props.withdrawFifty} type="button">
+            Withdraw $50
+          </button>
+
+          <button onClick={this.props.withdrawHundred} type="button">
+            Withdraw $100
+          </button>
+        </div>
+
+        <div className="atm">
+          <label htmlFor="firstName">Custom Amount: </label>
+
+          <input
+            type="text"
+            id="firstName"
+            required
+            pattern="[A-Za-z]{2,32}"
+            title="Must contain uppercase and lowercase letters only, and at least 2 characters in total"
+            onChange={this.handleChange}
+          />
+
+          <button onClick={this.props.depositCustomAmount} type="button">
+            Deposit
+          </button>
+
+          <button onClick={this.props.withdrawCustomAmount} type="button">
+            Withdraw
+          </button>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   balance: state.bankReducer.balance,
