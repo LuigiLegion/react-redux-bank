7632e6b5f5d9d96f2b46cdc2a21bc6d084577ce3
Tal Luigi <luigi.t.agr@gmail.com>
2020-06-09 16:43:30 -0400

    chore: refactored all app components to match updates made in solution branch

--- src/components/Bank.js
+++ src/components/Bank.js
@@ -1,40 +1,18 @@
+// Imports
 import React from 'react';
-import { connect } from 'react-redux';
 
-const Bank = props => {
+import Logo from './Logo';
+import Atm from './Atm';
+
+// Component
+function Bank() {
   return (
     <div>
-      <header>
-        <img className="logo" src="/logo.jpeg" alt="React-Redux Bank" />
-        <br />
-        <div>React-Redux Bank</div>
-      </header>
-      <br />
-      <h1 className="balance">$ 0</h1>
-      <div className="atm">
-        <button type="button">Deposit $50</button>
-        <button type="button">Deposit $100</button>
-        <button type="button">Withdraw $50</button>
-        <button type="button">Withdraw $100</button>
-      </div>
+      <Logo />
+
+      <Atm />
     </div>
   );
-};
+}
 
-// Please refactor mapStateToProps and mapDispatchToProps into implicitly returning functions rather than explicitly returning ones once you get everything up and running
-const mapStateToProps = state => {
-  console.log('state in mapStateToProps: ', state);
-
-  return {};
-};
-
-const mapDispatchToProps = dispatch => {
-  console.log('dispatch in mapDispatchToProps: ', dispatch);
-
-  return {};
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Bank);
+export default Bank;
