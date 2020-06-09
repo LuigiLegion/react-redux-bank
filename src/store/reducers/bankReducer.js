2230e20590fa6fc55f16d65fbddee1e8b7ab1018
Tal Luigi <luigi.t.agr@gmail.com>
2020-06-09 18:57:09 -0400

    feat: created balance in state and the deposit fifty action type, action creator, and reducer case in bankReducer

--- src/store/reducers/bankReducer.js
+++ src/store/reducers/bankReducer.js
@@ -1,14 +1,25 @@
 // Initial State
-const initialState = {};
+const initialState = {
+  balance: 0,
+};
 
 // Actions Types
+const DEPOSIT_FIFTY = 'DEPOSIT_FIFTY';
 
 // Action Creators
+export const depositFiftyActionCreator = () => ({
+  type: DEPOSIT_FIFTY,
+});
 
 // Reducer
 const bankReducer = (state = initialState, action) => {
   switch (action.type) {
     // Your cases here
+    case DEPOSIT_FIFTY:
+      return {
+        ...state,
+        balance: state.balance + 50,
+      };
 
     default:
       return state;
