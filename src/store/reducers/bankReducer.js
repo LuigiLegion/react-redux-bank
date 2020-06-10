25c5cb2c9cf7bde574a039ccab65c9033b65739e
Tal Luigi <luigi.t.agr@gmail.com>
2020-06-10 11:37:10 -0400

    feat: created the depositHundred, withdrawFifty, and withdrawHundred action types, action creators, and reducer cases in bankReducer

--- src/store/reducers/bankReducer.js
+++ src/store/reducers/bankReducer.js
@@ -5,10 +5,25 @@ const initialState = {
 
 // Actions Types
 const DEPOSIT_FIFTY = 'DEPOSIT_FIFTY';
+const DEPOSIT_HUNDRED = 'DEPOSIT_HUNDRED';
+const WITHDRAW_FIFTY = 'WITHDRAW_FIFTY';
+const WITHDRAW_HUNDRED = 'WITHDRAW_HUNDRED';
 
 // Action Creators
 export const depositFiftyActionCreator = () => ({
   type: DEPOSIT_FIFTY,
+});
+
+export const depositHundredActionCreator = () => ({
+  type: DEPOSIT_HUNDRED,
+});
+
+export const withdrawFiftyActionCreator = () => ({
+  type: WITHDRAW_FIFTY,
+});
+
+export const withdrawHundredActionCreator = () => ({
+  type: WITHDRAW_HUNDRED,
 });
 
 // Reducer
@@ -19,6 +34,24 @@ const bankReducer = (state = initialState, action) => {
       return {
         ...state,
         balance: state.balance + 50,
+      };
+
+    case DEPOSIT_HUNDRED:
+      return {
+        ...state,
+        balance: state.balance + 100,
+      };
+
+    case WITHDRAW_FIFTY:
+      return {
+        ...state,
+        balance: state.balance - 50,
+      };
+
+    case WITHDRAW_HUNDRED:
+      return {
+        ...state,
+        balance: state.balance - 100,
       };
 
     default:
